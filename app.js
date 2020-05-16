7b3192b32c28ab6e888cf9742f049c751743d71d
KarandeepSinghCoder <karandchandi@gmail.com>
2020-05-16 12:26:18 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 var express = require("express")
 var app = express();
 var bodyParser =require("body-parser");
+var mongoose=require("mongoose");
+mongoose
+.connect("mongodb://localhost/yel_camp", {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+	console.log("fail");
+});
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+var Campground = mongoose.model("Campground",campgroundSchema);
+Campground.create(
+        {
+            name:"salmon creek",
+            image:"https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c2c7ed7914fc1_340.jpg"
+        },function(err,campground){
+            if(err){
+                console.log("error");
+            }else{
+                console.log("New yelpcamp ");
+                console.log(Campground);
+            }
+        });
 var campgrounds=[
     {name:"salmon creek",image:"https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e5074417c2c7ed7914fc1_340.jpg"},
     {name:"Himaliya",image:"https://pixabay.com/get/57e8d0424a5bae14f1dc84609620367d1c3ed9e04e5074417c2c7ed7914fc1_340.jpg"},
